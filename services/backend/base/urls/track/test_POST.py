9a9a2537e501bacbc54576c05368ae950096654d
Alex Passalis <alexpassalis9@gmail.com>
2025-10-27 15:02:14 +0100

    feat: Added invalid_url test for the /track endpoint.

--- services/backend/base/urls/track/test_POST.py
+++ services/backend/base/urls/track/test_POST.py
@@ -4,11 +4,27 @@ from . import router
 client = TestClient(router)
 
 
-def test_track():
+def test_track_happy_path():
     response = client.post('/track', json={'url': 'http://example.com'})
 
     assert response.status_code == 201
     assert response.json() == {
         'message': 'URL tracked successfully',
         'url': 'http://example.com/',
+    }
+
+
+def test_track_invalid_url():
+    response = client.post('/track', json={'url': 'not-a-valid-url'})
+
+    assert response.status_code == 422
+    assert response.json() == {
+        'detail': [
+            {
+                'ctx': {'error': 'relative URL without a base'},
+                'loc': ['body', 'data', 'url'],
+                'msg': 'Input should be a valid URL, relative URL without a base',
+                'type': 'url_parsing',
+            },
+        ],
     }
