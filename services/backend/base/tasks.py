09bb5b15afd1bc8ba7b4a67fa3afc7c50429bd84
Alex Passalis <alexpassalis9@gmail.com>
2025-10-31 12:55:09 +0100

    fix: Using logger instead of print for the 3 tasks. Added watchdog for hot-reload of the beat and worker.

--- services/backend/base/tasks.py
+++ services/backend/base/tasks.py
@@ -1,19 +1,22 @@
 from celery import shared_task
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def run_every_minute():
-    print('Task running every minute')
+    logger.info('Task running every minute')
     pass
 
 
 @shared_task
 def run_every_hour():
-    print('Task running every hour')
+    logger.info('Task running every hour')
     pass
 
 
 @shared_task
 def run_every_day():
-    print('Task running every day')
+    logger.info('Task running every day')
     pass
